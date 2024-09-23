c3478ba612339a644b3dc4aae2ca5ec5414ad5d1
lvnvn177 <53424393+lvnvn177@users.noreply.github.com>
2024-09-23 20:28:02 +0900

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "iOS-Module",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "iOS-Module",
+            targets: ["iOS-Module"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "iOS-Module"),
+        .testTarget(
+            name: "iOS-ModuleTests",
+            dependencies: ["iOS-Module"]),
+    ]
+)
