bb64855110d4a65d9741509ccbaefa1fb90d6699
lvnvn177 <53424393+lvnvn177@users.noreply.github.com>
2024-09-27 01:06:10 +0900

    delete_log

--- Sources/iOS-Module/Data/DataManager.swift
+++ Sources/iOS-Module/Data/DataManager.swift
@@ -20,14 +20,14 @@ public class DataManager<T: Codable> {
         // JSON 인코딩이 잘 되었는지 확인
         if let encodedData = try? encoder.encode(items) {
             UserDefaults.standard.set(encodedData, forKey: itemsKey)
-            print("Items saved successfully.")
+//            print("Items saved successfully.", encodedData)
         } else {
             print("Failed to encode items.")
         }
         
         // 저장된 데이터 확인
         if let savedData = UserDefaults.standard.data(forKey: itemsKey) {
-            print("Saved data exists in UserDefaults: \(savedData)")
+//            print("Saved data exists in UserDefaults: \(savedData)")
         } else {
             print("No data found in UserDefaults for key: \(itemsKey)")
         }
@@ -36,11 +36,11 @@ public class DataManager<T: Codable> {
     // 데이터를 로드하는 함수
     public func loadItem() -> [T] {
         if let saveData = UserDefaults.standard.data(forKey: itemsKey) {
-            print("Saved data exists in UserDefaults: \(saveData)")
+//            print("Saved data exists in UserDefaults: \(saveData)")
             
             let decoder = JSONDecoder()
             if let decodedItems = try? decoder.decode([T].self, from: saveData) {
-                print("Decoded items: \(decodedItems)")
+//                print("Decoded items: \(decodedItems)")
                 return decodedItems
             } else {
                 print("Failed to decode saved data.")
