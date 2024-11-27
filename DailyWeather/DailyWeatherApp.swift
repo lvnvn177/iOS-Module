b195f31548da69d681d2caf75e615012272f15cf
lvnvn177 <53424393+lvnvn177@users.noreply.github.com>
2024-11-28 02:34:59 +0900

    Initial Commit

---
+++ DailyWeather/DailyWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyWeatherApp.swift
+//  DailyWeather
+//
+//  Created by 이영호 on 11/28/24.
+//
+
+import SwiftUI
+
+@main
+struct DailyWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
