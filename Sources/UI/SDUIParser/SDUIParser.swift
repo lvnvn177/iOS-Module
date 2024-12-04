c5958199bfd8d5d47e9b193046922495f61a92d4
lvnvn177 <53424393+lvnvn177@users.noreply.github.com>
2024-12-04 17:29:41 +0900

    SDUI 파서 add json load

--- Sources/UI/SDUIParser/SDUIParser.swift
+++ Sources/UI/SDUIParser/SDUIParser.swift
@@ -1,6 +1,12 @@
 import Foundation
 import SDUIComponent
 
+public enum SDUIParserError: Error {
+    case fileNotFound
+    case invalidData
+    case parsingError(Error)
+}
+
 public class SDUIParser {
     public static func parse(json: String) throws -> SDUIComponent { // Local, SDUIComponent의 구조 형태로 작성된 json -> SDUIComponent 변환
         guard let jsonData = json.data(using: .utf8) else {
@@ -21,6 +27,22 @@ public class SDUIParser {
             return try decoder.decode(SDUIComponent.self, from: jsonData)
         } catch {
             throw SDUIError.parsingFailed(error)
+        }
+    }
+
+    public static func loadFromJSON(fileName: String) throws -> SDUIComponent {
+        guard let url = Bundle.main.url(forResource: fileName, withExtension: "json") else {
+            throw SDUIParserError.fileNotFound
+        }
+        
+        guard let jsonData = try? Data(contentsOf: url) else {
+            throw SDUIParserError.invalidData
+        }
+        
+        do {
+            return try parse(jsonData: jsonData)
+        } catch {
+            throw SDUIParserError.parsingError(error)
         }
     }
 }
